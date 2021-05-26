7b90918fd0875266294e61ae1551a5e15f3f2db1
Mahera <maherafurniturewala1111@gmail.com>
2021-05-26 21:32:23 +0530

    create ejs files and render them through controllers

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,5 @@
 //module.exports.actionName=function(req,res){}
 
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is up for codeial!</h1>');
+    return res.render('home',{title:"Home"});
 }
